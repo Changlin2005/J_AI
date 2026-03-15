4338c7f30cef63c51cd3f231e9d4f43b6aa8417c
Changlin2005 <changlin.2005@qq.com>
2026-03-15 19:28:08 +0800

    第一次提交：rag,tool(tts),skills完成,状态机情绪转变未测试

--- src/test/java/com/lcl/ai/AiApplicationTests.java
+++ src/test/java/com/lcl/ai/AiApplicationTests.java
@@ -2,7 +2,6 @@ package com.lcl.ai;
 
 import com.lcl.ai.service.IGraphCheckpointService;
 import com.lcl.ai.service.impl.GraphCheckpointServiceImpl;
-import com.lcl.ai.utils.TTS;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,8 +11,7 @@ class AiApplicationTests {
 
     @Test
     void contextLoads() {
-        GraphCheckpointServiceImpl iGraphCheckpointService=new GraphCheckpointServiceImpl();
-        System.out.println(iGraphCheckpointService.queryUserHistory("1"));
+
     }
 
 }
